b4a00b873c5027f53b81c4a3e4f1e41a541ccbe6
kisb2 <kisb55@protonmail.com>
2021-02-20 10:50:06 +0100

    commit

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def disconnect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="3.21.169.88")  # rpi: 192.168.0.41 ec2: 3.21.169.88
+    socketio.run(app, host="ec2-3-21-169-88.us-east-2.compute.amazonaws.com")  # rpi: 192.168.0.41 ec2: 3.21.169.88
