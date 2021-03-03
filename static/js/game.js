2fbd893039e8a426876c8a8f4b82468e363e182a
kisb2 <kisb55@protonmail.com>
2021-03-03 17:51:02 +0100

    update

--- static/js/game.js
+++ static/js/game.js
@@ -59,11 +59,11 @@ function onTimerTick() {
 	for (var i = 0; i < allfood.length; i++){
 		if (collision(posx+25, posy+25, 25, Number(allfood[i].style.left.slice(0, -2))+5, Number(allfood[i].style.top.slice(0, -2))+5, 5)) {
 			let justeaten = allfood[i].style.left.slice(0, -2) + ":" + allfood[i].style.top.slice(0, -2);
-			eatenfoods.push(justeaten)
 			if (eatenfoods.includes(justeaten)){
 				continue;
 			} else {
 				socket.emit("eaten", justeaten);
+				eatenfoods.push(justeaten);
 			}
 		}
 	}
